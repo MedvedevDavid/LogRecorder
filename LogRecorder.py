778a23c3cb1d15ce08a80a56aeb145e23c1cfd53
MedvedevDavid <72654960+MedvedevDavid@users.noreply.github.com>
2022-04-30 09:07:34 +0200

    minor refactoring added

--- LogRecorder.py
+++ LogRecorder.py
@@ -2,43 +2,52 @@ import matplotlib.pyplot as plt
 import search_number_in_text_contant as miner
 
 
-file_to_read = open("putty20220427171405.txt", "r")
 
-input_string = file_to_read.read().split("\n")
-converter_obj_Pic_in = miner.TextNumberSearcher(input_string)
-converter_obj_Pic_in.set_text_to_be_find("Pi")
-converter_obj_Pic_in.generate_sum_and_array()
+def mine_controller_data(file_to_read):
+    input_string = file_to_read.read().split("\n")
+    converter_obj_Pic_in = miner.TextNumberSearcher(input_string)
+    converter_obj_Pic_in.set_text_to_be_find("Pi")
+    converter_obj_Pic_in.generate_sum_and_array()
 
-converter_obj_Rol_in = miner.TextNumberSearcher(input_string)
-converter_obj_Rol_in.set_text_to_be_find("Ri")
-converter_obj_Rol_in.generate_sum_and_array()
+    converter_obj_Rol_in = miner.TextNumberSearcher(input_string)
+    converter_obj_Rol_in.set_text_to_be_find("Ri")
+    converter_obj_Rol_in.generate_sum_and_array()
 
-converter_obj_Pic_out = miner.TextNumberSearcher(input_string)
-converter_obj_Pic_out.set_text_to_be_find("Po")
-converter_obj_Pic_out.generate_sum_and_array()
+    converter_obj_Pic_out = miner.TextNumberSearcher(input_string)
+    converter_obj_Pic_out.set_text_to_be_find("Po")
+    converter_obj_Pic_out.generate_sum_and_array()
 
-converter_obj_Rol_out = miner.TextNumberSearcher(input_string)
-converter_obj_Rol_out.set_text_to_be_find("Ro")
-converter_obj_Rol_out.generate_sum_and_array()
+    converter_obj_Rol_out = miner.TextNumberSearcher(input_string)
+    converter_obj_Rol_out.set_text_to_be_find("Ro")
+    converter_obj_Rol_out.generate_sum_and_array()
+    return input_string,converter_obj_Pic_in,converter_obj_Rol_in,converter_obj_Pic_out,converter_obj_Rol_out
 
 
+def mine_motor_data(input_string):
+    converter_obj_motor_1 = miner.TextNumberSearcher(input_string)
+    converter_obj_motor_1.set_text_to_be_find("M1")
+    converter_obj_motor_1.generate_sum_and_array()
 
-converter_obj_motor_1 = miner.TextNumberSearcher(input_string)
-converter_obj_motor_1.set_text_to_be_find("M1")
-converter_obj_motor_1.generate_sum_and_array()
+    converter_obj_motor_2 = miner.TextNumberSearcher(input_string)
+    converter_obj_motor_2.set_text_to_be_find("M2")
+    converter_obj_motor_2.generate_sum_and_array()
 
-converter_obj_motor_2 = miner.TextNumberSearcher(input_string)
-converter_obj_motor_2.set_text_to_be_find("M2")
-converter_obj_motor_2.generate_sum_and_array()
+    converter_obj_motor_3 = miner.TextNumberSearcher(input_string)
+    converter_obj_motor_3.set_text_to_be_find("M3")
+    converter_obj_motor_3.generate_sum_and_array()
 
-converter_obj_motor_3 = miner.TextNumberSearcher(input_string)
-converter_obj_motor_3.set_text_to_be_find("M3")
-converter_obj_motor_3.generate_sum_and_array()
+    converter_obj_motor_4 = miner.TextNumberSearcher(input_string)
+    converter_obj_motor_4.set_text_to_be_find("M4")
+    converter_obj_motor_4.generate_sum_and_array()
+    return converter_obj_motor_1,converter_obj_motor_2,converter_obj_motor_3,converter_obj_motor_4
 
-converter_obj_motor_4 = miner.TextNumberSearcher(input_string)
-converter_obj_motor_4.set_text_to_be_find("M4")
-converter_obj_motor_4.generate_sum_and_array()
 
+file_to_read = open("logs/putty.log", "r")
+
+input_string, converter_obj_Pic_in, converter_obj_Rol_in, converter_obj_Pic_out, converter_obj_Rol_out = mine_controller_data(file_to_read)
+converter_obj_motor_1, converter_obj_motor_2, converter_obj_motor_3, converter_obj_motor_4 = mine_motor_data(input_string)
+
+file_to_read.close()
 fig, ax = plt.subplots()
 
 ax.plot(converter_obj_Pic_in.get_element_list(), linestyle = "-", color = "black", label="Pic in")
@@ -52,5 +61,3 @@ ax.plot(converter_obj_motor_4.get_element_list(), linestyle = ":", color = "dark
 
 ax.legend()
 plt.show()
-
-file_to_read.close()
