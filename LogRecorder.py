dd60da18a56ebbe17b384841d1f26b7eb8682502
MedvedevDavid <72654960+MedvedevDavid@users.noreply.github.com>
2023-01-08 09:35:48 +0100

    refactored to remove duplications

--- LogRecorder.py
+++ LogRecorder.py
@@ -2,62 +2,25 @@ import matplotlib.pyplot as plt
 import search_number_in_text_contant as miner
 
 
-
-def mine_controller_data(file_to_read):
-    input_string = file_to_read.read().split("\n")
-    converter_obj_Pic_in = miner.TextNumberSearcher(input_string)
-    converter_obj_Pic_in.set_text_to_be_find("Pi")
-    converter_obj_Pic_in.generate_sum_and_array()
-
-    converter_obj_Rol_in = miner.TextNumberSearcher(input_string)
-    converter_obj_Rol_in.set_text_to_be_find("Ri")
-    converter_obj_Rol_in.generate_sum_and_array()
-
-    converter_obj_Pic_out = miner.TextNumberSearcher(input_string)
-    converter_obj_Pic_out.set_text_to_be_find("Po")
-    converter_obj_Pic_out.generate_sum_and_array()
-
-    converter_obj_Rol_out = miner.TextNumberSearcher(input_string)
-    converter_obj_Rol_out.set_text_to_be_find("Ro")
-    converter_obj_Rol_out.generate_sum_and_array()
-    return input_string,converter_obj_Pic_in,converter_obj_Rol_in,converter_obj_Pic_out,converter_obj_Rol_out
+def mine_data(input_string, data_to_be_mined):
+    converter_obj = miner.TextNumberSearcher(input_string)
+    converter_obj.set_text_to_be_find(data_to_be_mined)
+    converter_obj.generate_sum_and_array()
+    return converter_obj
 
 
-def mine_motor_data(input_string):
-    converter_obj_motor_1 = miner.TextNumberSearcher(input_string)
-    converter_obj_motor_1.set_text_to_be_find("M1")
-    converter_obj_motor_1.generate_sum_and_array()
-
-    converter_obj_motor_2 = miner.TextNumberSearcher(input_string)
-    converter_obj_motor_2.set_text_to_be_find("M2")
-    converter_obj_motor_2.generate_sum_and_array()
-
-    converter_obj_motor_3 = miner.TextNumberSearcher(input_string)
-    converter_obj_motor_3.set_text_to_be_find("M3")
-    converter_obj_motor_3.generate_sum_and_array()
-
-    converter_obj_motor_4 = miner.TextNumberSearcher(input_string)
-    converter_obj_motor_4.set_text_to_be_find("M4")
-    converter_obj_motor_4.generate_sum_and_array()
-    return converter_obj_motor_1,converter_obj_motor_2,converter_obj_motor_3,converter_obj_motor_4
-
-
+mined_data_dict = {}
 file_to_read = open("logs/putty.log", "r")
+input_string = file_to_read.read().split("\n")
+strings_to_be_mined_list = ["Ro", "Po", "Ri", "Pi", "M1", "M2", "M3","M4"]
 
-input_string, converter_obj_Pic_in, converter_obj_Rol_in, converter_obj_Pic_out, converter_obj_Rol_out = mine_controller_data(file_to_read)
-converter_obj_motor_1, converter_obj_motor_2, converter_obj_motor_3, converter_obj_motor_4 = mine_motor_data(input_string)
+for strings_to_be_mined in strings_to_be_mined_list:
+    mined_data_dict[strings_to_be_mined] = mine_data(input_string, strings_to_be_mined)
 
 file_to_read.close()
 fig, ax = plt.subplots()
-
-ax.plot(converter_obj_Pic_in.get_element_list(), linestyle = "-", color = "black", label="Pic in")
-ax.plot(converter_obj_Rol_in.get_element_list(), linestyle = "--", color = "blue", label="Roll in")
-ax.plot(converter_obj_Pic_out.get_element_list(), linestyle = "-.", color = "green", label="Pic out")
-ax.plot(converter_obj_Rol_out.get_element_list(), linestyle = ":", color = "red", label="Roll out")
-ax.plot(converter_obj_motor_1.get_element_list(), linestyle = "-.", color = "darkmagenta", label="Motor 1")
-ax.plot(converter_obj_motor_2.get_element_list(), linestyle = ":", color = "fuchsia", label="Motor 2")
-ax.plot(converter_obj_motor_3.get_element_list(), linestyle = ":", color = "gray", label="Motor 3")
-ax.plot(converter_obj_motor_4.get_element_list(), linestyle = ":", color = "darkgreen", label="Motor 4")
+for key in mined_data_dict:
+    ax.plot(mined_data_dict[key].get_element_list(), label=key)
 
 ax.legend()
 plt.show()
